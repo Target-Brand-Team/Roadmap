ac3346fba7f0c2ac86ca6be3f559fc2a18422447
Fabio Lanza <fabio.lanza4@gmail.com>
2024-05-01 17:50:21 -0700

    added new width

--- src/Pages/2024/Dashboard.jsx
+++ src/Pages/2024/Dashboard.jsx
@@ -11,7 +11,7 @@ const Dashboard = () => {
   const [showOwned, setShowOwned] = useState(true);
 
   return (
-    <div className="flex flex-col gap-2 mt-1 mx-1 h-[2200px] max-[1200px]:w-[1016px]"> 
+    <div className="flex flex-col gap-2 mt-1 mx-1 h-[2200px] max-[1280px]:w-[1050px]"> 
       <div className="bg-red-600 text-white rounded-sm py-1 px-3 font-bold text-lg flex justify-center">
         2024
       </div>
